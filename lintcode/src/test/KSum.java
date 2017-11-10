ed4ad2df360cced1f82b8fe643645f699740e77e
Administrator <Administrator@ITHPHONXVZ2KT2S.lan>
2017-11-10 17:56:53 +0800

    k数之和，未完待续

---
+++ lintcode/src/test/KSum.java
@@ -0,0 +1,15 @@
+package test;
+
+public class KSum {
+	
+	public static int kSum(int[] A, int k, int target) {
+		
+        return 0;
+    }
+
+	public static void main(String[] args) {
+		int[] A = {1,2,3,4};
+		System.out.println(kSum(A, 2, 5));
+	}
+
+}
